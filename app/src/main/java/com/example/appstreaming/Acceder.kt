974faf5d479863ed0bd829b407147793b9ca2fe8
JosGon615 <josgon615@alu.centroafuera.es>
2023-02-20 13:25:05 +0100

    4

---
+++ app/src/main/java/com/example/appstreaming/Acceder.kt
@@ -0,0 +1,11 @@
+package com.example.appstreaming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Acceder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acceder)
+    }
+}
--- app/src/main/java/com/example/appstreaming/MainActivity.kt
+++ app/src/main/java/com/example/appstreaming/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.appstreaming
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import com.example.appstreaming.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        textColor()
+
     }
+    private fun textColor(){
+        val spannableString = SpannableString("o Crear Cuenta")
+        val fcolor = ForegroundColorSpan(Color.parseColor("#f47521"))
+        spannableString.setSpan(fcolor, 2, 14, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.crearCuenta.text = spannableString
+    }
+
+
 }
