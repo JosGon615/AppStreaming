c0e0e2358da486a88b7856b431cb2cc02014f0f5
JosGon615 <josgon615@alu.centroafuera.es>
2023-02-20 14:13:20 +0100

    5

--- app/src/main/java/com/example/appstreaming/Acceder.kt
+++ app/src/main/java/com/example/appstreaming/Acceder.kt
@@ -1,11 +1,36 @@
 package com.example.appstreaming
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appstreaming.databinding.ActivityAccederBinding
+
 
 class Acceder : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAccederBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_acceder)
+        binding = ActivityAccederBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cross.setOnClickListener {
+            val intent = Intent(this@Acceder, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        //cambiar backgroundTint de los editText al presionarlos
+        binding.user.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding.user.setBackgroundColor(Color.parseColor("#f47521"))
+            } else {
+                binding.user.setBackgroundColor(Color.parseColor("#57585a"))
+            }
+        }
+
     }
+
+
+
 }
--- app/src/main/java/com/example/appstreaming/MainActivity.kt
+++ app/src/main/java/com/example/appstreaming/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.appstreaming
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,6 +17,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         textColor()
+
+
+
+        binding.explorar.setOnClickListener {
+            val intent = Intent(this@MainActivity, Explorar::class.java)
+            startActivity(intent)
+        }
+        binding.acceder.setOnClickListener {
+            val intent = Intent(this@MainActivity, Acceder::class.java)
+            startActivity(intent)
+        }
 
     }
     private fun textColor(){
